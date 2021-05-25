2e20898da804f2dd44ee280046a632f36cd320d8
eduardorovere <eduardo.rovere@gmail.com>
2021-05-24 22:39:34 -0300

    chore(first-commit): Angular base project, first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('pokestore-b2w app is running!');
+    expect(page.getTitleText()).toEqual('pokestore-b2wdigital app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokestore-b2w'`, () => {
+  it(`should have as title 'pokestore-b2wdigital'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokestore-b2w');
+    expect(app.title).toEqual('pokestore-b2wdigital');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pokestore-b2w app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('pokestore-b2wdigital app is running!');
   });
 });
