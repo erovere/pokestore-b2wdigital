78626cd18deeea062fe75f84f8ee59f69fb5cedc
eduardorovere <eduardo.rovere@gmail.com>
2021-05-26 13:19:01 -0300

    feat(mobile): Mobile adjusts started.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,15 +20,15 @@ export class AppComponent implements OnInit {
     isLoadingConfiguration: boolean;
 
     ngOnInit(): void {
-        this.whoami();
         this.configureApplication();
     }
 
     private configureApplication(): void {
         this.isLoadingConfiguration = true;
+        const url = new URL(window.location.href);
+        this.tenantId = hostTenantMap[url.host];        
         this.configurationService.disableCache();
-        // this.configurationService.enableCache();
-
+        // this.configurationService.enableCache();    
         this.loadConfiguration().subscribe((data: any) => {
             this.themingService.setCSSVariables(data[this.tenantId].theme);
             this.isLoadingConfiguration = false;
@@ -38,10 +38,5 @@ export class AppComponent implements OnInit {
     private loadConfiguration(): Observable<AppConfiguration> {
         this.configurationService.tenantId = this.tenantId;
         return this.configurationService.getConfig();
-    }
-
-    private whoami(): void {
-        const url = new URL(window.location.href);
-        this.tenantId = hostTenantMap[url.host];
     }
 }
