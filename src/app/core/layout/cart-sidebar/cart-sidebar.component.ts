4d2d7d30d2ed3add5e3a059eefb9f99ab6773437
eduardorovere <eduardo.rovere@gmail.com>
2021-05-28 01:00:26 -0300

    fix(fix-mobile-layout): Fixing mobile issues in CSS

--- src/app/core/layout/cart-sidebar/cart-sidebar.component.ts
+++ src/app/core/layout/cart-sidebar/cart-sidebar.component.ts
@@ -23,7 +23,9 @@ export class CartSidebarComponent {
         private analytics: AnalyticsService,
         private router: Router,
         private dialog: MatDialog
-    ) { }
+    ) {
+        this.onResize();
+     }
 
     @HostListener('window:resize', ['$event'])
     onResize(event?) {
@@ -59,6 +61,10 @@ export class CartSidebarComponent {
     get getTotalPrice(): number {
         this.totalPrice = this.cartService.getTotalPrice();
         return this.totalPrice;
+    }
+
+    public toggleCartVisibility() {
+        this.cartService.toggleCartVisibility();
     }
 
     public completePurchase(): void {
